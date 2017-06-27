fe77a9a80332613c9d9f8057ddf78ae5a3e0e675
HuangLihong <peterlihong@qq.com>
2017-06-27 15:15:45 +0800

    Add cgminer.default.hash12.h3 and cgminer.hash12.h3

--- upgradeForPacking.sh
+++ upgradeForPacking.sh
@@ -9,6 +9,7 @@ fi
 # Target dir to be upgraded is openwrt/build_dir/target-arm_cortex-a8+vfpv3_musl-1.1.15_eabi/root-sunxi
 upgrade_dir=$1
 target_dir=$2
+tmp_src_dir=$upgrade_dir/upgrade-files/tmp-rootfs
 
 #
 # upgrade target_dir with patch_dir
@@ -16,16 +17,18 @@ target_dir=$2
 echo ""
 echo "Upgrading $target_dir by $upgrade_dir"
 
-# Remove unused files under $upgrade_dir
-rm -f $upgrade_dir/upgrade-files/rootfs/usr/bin/cgminer
-rm -f $upgrade_dir/upgrade-files/rootfs/usr/bin/cgminer-api
-find $upgrade_dir/upgrade-files/rootfs/usr/lib/lua/ -name *.so | xargs rm -f
+cp -af $upgrade_dir/upgrade-files/rootfs $tmp_src_dir
 
-cp $upgrade_dir/upgrade-files/rootfs/bin/bitmicro-test.h3 $upgrade_dir/upgrade-files/rootfs/bin/bitmicro-test
-cp $upgrade_dir/upgrade-files/rootfs/usr/bin/keyd.h3 $upgrade_dir/upgrade-files/rootfs/usr/bin/keyd
-cp $upgrade_dir/upgrade-files/rootfs/usr/bin/readpower.h3 $upgrade_dir/upgrade-files/rootfs/usr/bin/readpower
-cp $upgrade_dir/upgrade-files/rootfs/usr/bin/setpower.h3 $upgrade_dir/upgrade-files/rootfs/usr/bin/setpower
-cp $upgrade_dir/upgrade-files/rootfs/usr/bin/temp-monitor.h3 $upgrade_dir/upgrade-files/rootfs/usr/bin/temp-monitor
+# Remove unused files under $tmp_src_dir
+rm -f $tmp_src_dir/usr/bin/cgminer
+rm -f $tmp_src_dir/usr/bin/cgminer-api
+find $tmp_src_dir/usr/lib/lua/ -name *.so | xargs rm -f
+
+mv $tmp_src_dir/bin/bitmicro-test.h3 $tmp_src_dir/bin/bitmicro-test
+mv $tmp_src_dir/usr/bin/keyd.h3 $tmp_src_dir/usr/bin/keyd
+mv $tmp_src_dir/usr/bin/readpower.h3 $tmp_src_dir/usr/bin/readpower
+mv $tmp_src_dir/usr/bin/setpower.h3 $tmp_src_dir/usr/bin/setpower
+mv $tmp_src_dir/usr/bin/temp-monitor.h3 $tmp_src_dir/usr/bin/temp-monitor
 
 # Remove unused files under $target_dir/etc
 rm -f $target_dir/etc/config/firewall
@@ -33,7 +36,9 @@ rm -f $target_dir/etc/init.d/om-watchdog
 rm -f $target_dir/etc/rc.d/S11om-watchdog
 rm -f $target_dir/etc/rc.d/K11om-watchdog
 
-cp -af $upgrade_dir/upgrade-files/rootfs/* $target_dir/
+cp -af $tmp_src_dir/* $target_dir/
+
+#rm -rf $tmp_src_dir
 
 echo ""
 echo "Done"

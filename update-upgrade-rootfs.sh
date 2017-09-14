0acab0cf345973fb2a627f9fb3facc11d623efcd
weijianli75 <peter@microbt.com>
2017-09-14 17:03:06 +0800

    Remove upgrade-rootfs/h3-rootfs/etc/boot.md5

--- update-upgrade-rootfs.sh
+++ update-upgrade-rootfs.sh
@@ -12,34 +12,18 @@ upgrade_files_tmp_dir=/tmp/upgrade-files-tmp
 #
 rm -fr $upgrade_files_tmp_dir
 mkdir -p $upgrade_files_tmp_dir
+cp -af upgrade-files/rootfs $upgrade_files_tmp_dir
 
 # Remove useless files
-cp -af upgrade-files/rootfs $upgrade_files_tmp_dir
 find $upgrade_files_tmp_dir/rootfs -name "*.h3" | xargs rm -f
 rm -f $upgrade_files_tmp_dir/rootfs/etc/rc.d/S90phonixtest
 rm -f $upgrade_files_tmp_dir/rootfs/etc/init.d/phonixtest
 rm -f $upgrade_files_tmp_dir/rootfs/usr/bin/phonixtest
 rm -f $upgrade_files_tmp_dir/rootfs/etc/rc.local
-
 rm -fr upgrade-rootfs/zynq-rootfs/usr/lib/lua
+
+# Copy ...
 cp -af $upgrade_files_tmp_dir/rootfs/* upgrade-rootfs/zynq-rootfs/
-
-#
-# 2. Update upgrade-rootfs/h3-rootfs/
-#
-rm -fr $upgrade_files_tmp_dir
-mkdir -p $upgrade_files_tmp_dir
-
-cp -af upgrade-files/rootfs $upgrade_files_tmp_dir
-
-for file in $(find $upgrade_files_tmp_dir/rootfs -name "*.h3")
-do
-    newfile=`echo $file | sed 's/\.h3$//'`
-    mv $file $newfile
-done
-
-rm -fr upgrade-rootfs/h3-rootfs/usr/lib/lua
-cp -af $upgrade_files_tmp_dir/rootfs/* upgrade-rootfs/h3-rootfs/
 
 # These files will not be upgraded
 rm -f upgrade-rootfs/zynq-rootfs/etc/config/pools
@@ -47,10 +31,30 @@ rm -f upgrade-rootfs/zynq-rootfs/etc/config/network
 rm -f upgrade-rootfs/zynq-rootfs/etc/shadow
 rm -f upgrade-rootfs/zynq-rootfs/etc/shadow-
 
+#
+# 2. Update upgrade-rootfs/h3-rootfs/
+#
+rm -fr $upgrade_files_tmp_dir
+mkdir -p $upgrade_files_tmp_dir
+cp -af upgrade-files/rootfs $upgrade_files_tmp_dir
+
+# Remove useless files
+for file in $(find $upgrade_files_tmp_dir/rootfs -name "*.h3")
+do
+    newfile=`echo $file | sed 's/\.h3$//'`
+    mv $file $newfile
+done
+rm -fr upgrade-rootfs/h3-rootfs/usr/lib/lua
+
+# Copy ...
+cp -af $upgrade_files_tmp_dir/rootfs/* upgrade-rootfs/h3-rootfs/
+
+# These files will not be upgraded
 rm -f upgrade-rootfs/h3-rootfs/etc/config/pools
 rm -f upgrade-rootfs/h3-rootfs/etc/config/network
 rm -f upgrade-rootfs/h3-rootfs/etc/shadow
 rm -f upgrade-rootfs/h3-rootfs/etc/shadow-
+rm -f upgrade-rootfs/h3-rootfs/etc/boot.md5
 
 # Delete tmp dir
 rm -fr $upgrade_files_tmp_dir

f7c7154c35eb113bd3b42534ff33b5bf83f19ec4
weijianli75 <peter@microbt.com>
2017-11-06 09:57:58 +0800

    Release 20171106.10.1

--- make-package.sh
+++ make-package.sh
@@ -9,10 +9,7 @@ UPGRADE_FILES_COMMON_PACKAGENAME=upgrade-whatsminer-$MACHINE_TYPE-common-$VERSIO
 UPGRADE_FILES_ZYNQ_PACKAGENAME=upgrade-whatsminer-$MACHINE_TYPE-zynq-$VERSION_NUMBER.tgz
 UPGRADE_FILES_H3_PACKAGENAME=upgrade-whatsminer-$MACHINE_TYPE-h3-$VERSION_NUMBER.tgz
 
-rm -f whatsminer-*.zip upgrade-whatsminer*.tgz
-
-# Create tmp dir
-cp -af upgrade-bin .upgrade-bin-bak
+rm -f upgrade-whatsminer*.tgz
 
 # Generate UPGRADE_FULL_PACKAGENAME
 ./update-upgrade-rootfs.sh
@@ -21,27 +18,6 @@ tar zcf $UPGRADE_FULL_PACKAGENAME upgrade.sh upgrade-bin upgrade-rootfs
 # Generate UPGRADE_FILES_COMMON_PACKAGENAME
 tar zcf $UPGRADE_FILES_COMMON_PACKAGENAME upgrade.sh upgrade-bin upgrade-files
 
-# Generate UPGRADE_FILES_ZYNQ_PACKAGENAME
-rm -f upgrade-bin/boot.fex upgrade-bin/old-boot.fex upgrade-bin/BOOT-ZYNQ10.bin upgrade-bin/BOOT-ZYNQ12.bin upgrade-bin/uImage
-echo "ZYNQ" > package-type
-tar zcf $UPGRADE_FILES_ZYNQ_PACKAGENAME package-type upgrade.sh upgrade-bin upgrade-files
-
-# Generate UPGRADE_FILES_H3_PACKAGENAME
-cp -f .upgrade-bin-bak/* upgrade-bin/
-rm -f upgrade-bin/BOOT-ZYNQ10.bin upgrade-bin/BOOT-ZYNQ12.bin upgrade-bin/devicetree.dtb upgrade-bin/uImage
-echo "H3" > package-type
-tar zcf $UPGRADE_FILES_H3_PACKAGENAME package-type upgrade.sh upgrade-bin upgrade-files
-
-# Restore upgrade-bin and remove tmp dir
-cp -f .upgrade-bin-bak/* upgrade-bin/
-rm -fr .upgrade-bin-bak
-rm -f package-type
-
-#WHATSMINER_PACKAGE_NAME=whatsminer-$MACHINE_TYPE-$VERSION_NUMBER-upgrade
-#zip $WHATSMINER_PACKAGE_NAME.zip HOWTO remote-upgrade.sh $UPGRADE_FILES_PACKAGENAME
-
 echo "Generated packages:"
 echo "  $UPGRADE_FULL_PACKAGENAME"
 echo "  $UPGRADE_FILES_COMMON_PACKAGENAME"
-echo "  $UPGRADE_FILES_ZYNQ_PACKAGENAME"
-echo "  $UPGRADE_FILES_H3_PACKAGENAME"
--- upgrade-rootfs/h3-rootfs/lib/preinit/01_preinit_sunxi.sh
+++ upgrade-rootfs/h3-rootfs/lib/preinit/01_preinit_sunxi.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-
-do_sunxi() {
-        . /lib/sunxi.sh
-
-        sunxi_board_detect
-}
-
-boot_hook_add preinit_main do_sunxi
+# do nothing

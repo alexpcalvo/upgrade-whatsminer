f60f5862a08bc92a8aefee24fc4d114583af0b49
weijianli75 <peter@microbt.com>
2017-12-06 10:43:19 +0800

    Change package name upgrade-whatsminer-m1-m1s-m2-m3-common-xxx.tgz to upgrade-whatsminer--common-xxx.tgz

--- make-package.sh
+++ make-package.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-MACHINE_TYPE="m1-m1s-m2-m3"
-
 VERSION_NUMBER=`cat upgrade-files/rootfs/etc/microbt_release | grep FIRMWARE_VERSION | cut -d"=" -f2 | sed "s/'//g"`
 
 UPGRADE_FULL_PACKAGENAME=upgrade-whatsminer-full-$VERSION_NUMBER.tgz
-UPGRADE_FILES_COMMON_PACKAGENAME=upgrade-whatsminer-$MACHINE_TYPE-common-$VERSION_NUMBER.tgz
-UPGRADE_FILES_ZYNQ_PACKAGENAME=upgrade-whatsminer-$MACHINE_TYPE-zynq-$VERSION_NUMBER.tgz
-UPGRADE_FILES_H3_PACKAGENAME=upgrade-whatsminer-$MACHINE_TYPE-h3-$VERSION_NUMBER.tgz
+UPGRADE_FILES_COMMON_PACKAGENAME=upgrade-whatsminer-common-$VERSION_NUMBER.tgz
 
 rm -f upgrade-whatsminer*.tgz
 

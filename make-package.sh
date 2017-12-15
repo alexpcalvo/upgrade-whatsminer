32edce55e539d4e40f9f1a97a7ec44ad2e675ef5
weijianli75 <peter@microbt.com>
2017-12-15 12:07:12 +0800

    Add update-release-version.sh and update upgrade-rootfs/*

--- make-package.sh
+++ make-package.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$1" == "fv" ]; then
+    echo "Making fixed-voltage version packages"
+    ./update-release-version.sh
+else
+    echo "Making auto-voltage version packages"
+fi
+
 VERSION_NUMBER=`cat upgrade-files/rootfs/etc/microbt_release | grep FIRMWARE_VERSION | cut -d"=" -f2 | sed "s/'//g"`
 
 UPGRADE_FULL_PACKAGENAME=upgrade-whatsminer-full-$VERSION_NUMBER.tgz
@@ -13,6 +20,10 @@ tar zcf $UPGRADE_FULL_PACKAGENAME upgrade.sh upgrade-bin upgrade-rootfs
 
 # Generate UPGRADE_FILES_COMMON_PACKAGENAME
 tar zcf $UPGRADE_FILES_COMMON_PACKAGENAME upgrade.sh upgrade-bin upgrade-files
+
+if [ "$1" == "fv" ]; then
+    ./update-release-version.sh restore
+fi
 
 echo "Generated packages:"
 echo "  $UPGRADE_FULL_PACKAGENAME"
